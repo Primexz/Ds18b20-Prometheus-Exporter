ba40528d66c94d325e479642feafde110c3b4d8e
Johann <76482511+Primexz@users.noreply.github.com>
2023-01-12 10:45:30 +0100

    I love snake_case

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from prometheus_client import start_http_server, Gauge
 import time
 
-sensePath = "/sys/bus/w1/devices/28-012062361005/w1_slave"
-refreshInterval = 2
+sens_path = "/sys/bus/w1/devices/28-012062361005/w1_slave"
+interval = 2
 
-prometheusTemperature = Gauge('temperature', 'currTemp')
+prometheus_temp = Gauge('temperature', 'currTemp')
 
-def readTemperature():
-    file = open(sensePath, "r")
+def read_temp():
+    file = open(sens_path, "r")
     temperature = file.readlines()
     while temperature[0].strip()[-3:] != "YES":
         time.sleep(100)
 
     return temperature[1].split("=")[1]
 
-def publishData(temp):
-    prometheusTemperature.set(temp)
+def publish_data(temp):
+    prometheus_temp.set(temp)
 
 if __name__ == '__main__':
     start_http_server(9898, "127.0.0.1")
     while True:
-        temp = readTemperature()
-        publishData(temp)
-        time.sleep(refreshInterval)
+        temp = read_temp()
+        publish_data(temp)
+        time.sleep(interval)
